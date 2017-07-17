cdecc42f6b28990f7e44879227ee9f6927a2afdb
Camilo Soto Montoya <csotomon@hotmail.com>
2017-07-17 08:22:45 -0500

    starter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StarterComponent } from './starter/starter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/starter/starter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starter',
+  templateUrl: './starter.component.html',
+  styleUrls: ['./starter.component.css']
+})
+export class StarterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
