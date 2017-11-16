2bfd56459752a20c897385d6dd8bacc281ac14da
Phantase <phantase@free.fr>
2017-11-16 21:40:04 +0100

    Correct spec file of AdminComponent

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AdminComponent } from './admin.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
+import { AdminLeftSideComponent } from './admin-left-side/admin-left-side.component';
+import { AdminFooterComponent } from './admin-footer/admin-footer.component';
+import { AdminControlSidebarComponent } from './admin-control-sidebar/admin-control-sidebar.component';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,7 +14,16 @@ describe('AdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      imports: [ 
+        RouterTestingModule 
+      ],
+      declarations: [ 
+        AdminComponent,
+        AdminHeaderComponent,
+        AdminLeftSideComponent,
+        AdminFooterComponent,
+        AdminControlSidebarComponent
+      ]
     })
     .compileComponents();
   }));
