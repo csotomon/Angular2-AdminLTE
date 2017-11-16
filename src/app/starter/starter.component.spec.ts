0d957a9b2abf3c14a13ab7b09947fbee77e81693
Phantase <phantase@free.fr>
2017-11-16 21:44:26 +0100

    Correct spec file of StarterComponent

--- src/app/starter/starter.component.spec.ts
+++ src/app/starter/starter.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { StarterComponent } from './starter.component';
+import { StarterHeaderComponent } from './starter-header/starter-header.component';
+import { StarterLeftSideComponent } from './starter-left-side/starter-left-side.component';
+import { StarterContentComponent } from './starter-content/starter-content.component';
+import { StarterFooterComponent } from './starter-footer/starter-footer.component';
+import { StarterControlSidebarComponent } from './starter-control-sidebar/starter-control-sidebar.component';
 
 describe('StarterComponent', () => {
   let component: StarterComponent;
@@ -8,7 +15,17 @@ describe('StarterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StarterComponent ]
+      imports: [ 
+        RouterTestingModule 
+      ],
+      declarations: [ 
+        StarterComponent,
+        StarterHeaderComponent,
+        StarterLeftSideComponent,
+        StarterContentComponent,
+        StarterFooterComponent,
+        StarterControlSidebarComponent
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +39,11 @@ describe('StarterComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(StarterComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Page Header');
+  }));
 });
