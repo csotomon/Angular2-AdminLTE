ca0a9131642dd7919495f8185b753e6093e8218f
Camilo Soto Montoya <csotomon@hotmail.com>
2018-03-21 23:16:57 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'heroes', loadChildren: 'LoginComponent' }
+];
+
 @NgModule({
- exports: [
-   RouterModule,
- ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [
+    RouterModule,
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ],
+  declarations: []
+})
+export class LoginModule { }
